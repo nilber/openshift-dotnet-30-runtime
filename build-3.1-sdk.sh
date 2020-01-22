debcffb8ef1f75ea7a867ff8088a3fc483289006
Nilber <nilber@vittorazzi.com>
2020-01-22 15:31:31 -0300

    add .net core 3.1

---
+++ build-3.1-sdk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOCKER_IMAGEM=dotnet-31-sdk-alpine
+S2I_IMAGEM=nbmaster/openshift-dotnet-31-sdk-alpine
+TAG=3.1-sdk
+
+# docker system prune
+# docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+
+docker build src-3.1-sdk/ -t $DOCKER_IMAGEM:$TAG
+
+s2i build src-3.1-sdk/ $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+
+docker push $S2I_IMAGEM:$TAG
